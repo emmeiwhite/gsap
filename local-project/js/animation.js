ce44d2d13b3dfbc69aaa32a68b759f5b01068f2a
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-12-22 15:59:36 +0530

    emmei: from(), fromTo() tween methods

--- local-project/js/animation.js
+++ local-project/js/animation.js
@@ -1,4 +1,5 @@
-gsap.to(".box", { x: 400, rotate: 27, duration: 5 });
+// gsap.to(".box", { x: 400, rotate: 27, duration: 5 });
+gsap.from(".box", { x: 400, rotate: 27, duration: 5 });
 
 // tween methods
 var tween = gsap.to(".pink", {
@@ -14,3 +15,11 @@ document.querySelector("#pause").onclick = () => tween.pause();
 document.querySelector("#resume").onclick = () => tween.resume();
 document.querySelector("#reverse").onclick = () => tween.reverse();
 document.querySelector("#restart").onclick = () => tween.restart();
+
+// from(), fromTo()
+gsap.from(".blue", { x: 700, rotate: 40, duration: 4 });
+gsap.fromTo(
+  ".purple",
+  { x: 700, y: -30, rotate: 40, duration: 4 },
+  { x: 0, y: 10, duration: 5, rotate: 90 }
+);
