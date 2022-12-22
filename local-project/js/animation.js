28f962ca21aaa64b2bf1f54c694c32fda309a181
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-12-22 12:44:48 +0530

    emmei: tween basic methods and js event handlers

--- local-project/js/animation.js
+++ local-project/js/animation.js
@@ -1,1 +1,16 @@
 gsap.to(".box", { x: 400, rotate: 27, duration: 5 });
+
+// tween methods
+var tween = gsap.to(".pink", {
+  duration: 4,
+  x: 750,
+  rotation: 360,
+  ease: "none",
+  paused: true,
+});
+
+document.querySelector("#play").onclick = () => tween.play();
+document.querySelector("#pause").onclick = () => tween.pause();
+document.querySelector("#resume").onclick = () => tween.resume();
+document.querySelector("#reverse").onclick = () => tween.reverse();
+document.querySelector("#restart").onclick = () => tween.restart();
